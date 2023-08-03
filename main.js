55f580da001a74c0bbfabddcf89c4c949218f2f6
Sebas Ariza <sebadia1333@gmail.com>
2023-08-03 13:18:14 -0500

    union del mobile menu y del aside del carrito de compras, ademas se optimizo el codigo de js

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const navbarEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart-icon');
+const aside = document.querySelector('.product-detail');
 
-navbarEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleClass);
+burgerIcon.addEventListener('click', toggleClass);
+menuCarritoIcon.addEventListener('click', toggleClass);
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
-    console.log('click')
+function toggleClass(event){
+    if (event.target === menuEmail){
+        desktopMenu.classList.toggle('inactiveY');
+        aside.classList.add('inactiveY');
+    }else if (event.target === burgerIcon){
+        // mobileMenu.classList.toggle('inactiveX');
+        mobileMenu.classList.toggle('inactiveX');
+        aside.classList.add('inactiveY');
+    }else if (event.target === menuCarritoIcon){
+        // aside.classList.toggle('inactiveY');
+        aside.classList.toggle('inactiveY');
+        mobileMenu.classList.add('inactiveX');
+        desktopMenu.classList.add('inactiveY');
+    }
 }
-
-
-
