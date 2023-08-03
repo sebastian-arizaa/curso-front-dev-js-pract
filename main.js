c9288972b6bee66ef2ea2c98b289d55769b56bb9
Sebas Ariza <sebadia1333@gmail.com>
2023-08-03 09:42:28 -0500

    primer commit y union del menu con la navbar ademas de su interactividad en la vista desktop

---
+++ main.js
@@ -0,0 +1,12 @@
+const navbarEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navbarEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+    console.log('click')
+}
+
+
+
