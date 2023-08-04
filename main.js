3a41416657cc278649b367cbc6e4d54e292f06de
Sebas Ariza <sebadia1333@gmail.com>
2023-08-04 11:24:47 -0500

    juntando los dos product details, se cambio el nombre de uno a shopping-cart-container y por ende todo lo relacionado con el en js css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart-icon');
-const aside = document.querySelector('.product-detail');
+const shoppingCarContainer = document.querySelector('#shopping-cart-container');
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleClass);
@@ -13,12 +13,12 @@ menuCarritoIcon.addEventListener('click', toggleClass);
 function toggleClass(event){
     if (event.target === menuEmail){
         desktopMenu.classList.toggle('inactiveY');
-        aside.classList.add('inactiveY');
+        shoppingCarContainer.classList.add('inactiveY');
     }else if (event.target === burgerIcon){
         mobileMenu.classList.toggle('inactiveX');
-        aside.classList.add('inactiveY');
+        shoppingCarContainer.classList.add('inactiveY');
     }else if (event.target === menuCarritoIcon){
-        aside.classList.toggle('inactiveY');
+        shoppingCarContainer.classList.toggle('inactiveY');
         mobileMenu.classList.add('inactiveX');
         desktopMenu.classList.add('inactiveY');
         console.log(event)
